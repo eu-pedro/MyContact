1458d6d3077e13dddef2e5ec3785a332f58e5043
pedro <pedro.henrike2812@gmail.com>
2022-06-17 13:31:12 -0300

    Finalizado projeto my contact

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ const app = Vue.createApp({
   data(){
     return{
         nome: '',
-        email: '',
         whats: '',
         msg1: '',
-        msg2: '',
         msg3: '',
          
     }
@@ -18,17 +16,7 @@ const app = Vue.createApp({
       } else{
           this.msg1 = 'verifique seus dados e tente novamente!'
       } 
-      if(this.email != ''){
-          this.msg2 = 'campo válido'
-      } else{
-          this.msg2 = 'verifique seus dados e tente novamente!'
-      } 
-      if(this.whats == ''){
-        this.msg3 = 'verifique seus dados e tente novamente!'
-      }
-      else{
-        this.msg3 = 'campo válido!'
-      }
+      
 
       
     }
